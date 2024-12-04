cb578e9729e97d6a84bd44873b94183acbfd1e18
LeonidElkov <148034601+LeonidElkov@users.noreply.github.com>
2024-12-05 02:13:39 +0500

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,20 @@
+public class Cat implements Meowable {
+    private final String name; // Имя кота
+
+    // Конструктор
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    // Метод для получения текстового представления кота
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    // Реализация метода meow
+    @Override
+    public void meow() {
+        System.out.println(name + ": мяу!");
+    }
+}
