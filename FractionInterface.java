54fb8d1ee0c7eebbc9a9addd27770b7312a5c7cf
LeonidElkov <148034601+LeonidElkov@users.noreply.github.com>
2024-12-05 02:15:30 +0500

    Create FractionInterface.java

---
+++ FractionInterface.java
@@ -0,0 +1,5 @@
+public interface FractionInterface {
+    double toDouble();         // Получение вещественного значения
+    void setNumerator(int numerator); // Установка числителя
+    void setDenominator(int denominator); // Установка знаменателя
+}
