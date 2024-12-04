ea01ca1191d8f168cfcd6741effe6315f39f820b
LeonidElkov <148034601+LeonidElkov@users.noreply.github.com>
2024-12-05 02:17:53 +0500

    Create Polyline.java

---
+++ Polyline.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Polyline {
+    private final List<Point> points;
+
+    public Polyline(List<Point> points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "Линия " + points;
+    }
+}
