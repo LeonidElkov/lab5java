06eb83bb231cc55be495b2cab04e3d3024bf9ff6
LeonidElkov <148034601+LeonidElkov@users.noreply.github.com>
2024-12-13 00:56:44 +0500

    Create FractionOperations.java

---
+++ FractionOperations.java
@@ -0,0 +1,5 @@
+public interface FractionOperations {
+    double getValue(); // Получение вещественного значения
+    void setNumerator(int numerator); // Установка числителя
+    void setDenominator(int denominator); // Установка знаменателя
+}
